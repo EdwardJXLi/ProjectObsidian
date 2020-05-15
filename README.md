491460bde88984216badeb64f272515800ddaaff
Edward Li <radioactivehydra@gmail.com>
2020-05-14 23:15:43 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Obsidian
+
+Minecraft Classic 0.30 (Server Version 1.10.1; Protocol Version 7) Server Reverse Engineer Project
---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import argparse
+
+def main():
+    #Initiate Argument Parser
+    parser = argparse.ArgumentParser(description="TODO")
+
+    args = parser.parse_args()
+
+    #Create Server Instance
+    #server = MinecraftServer()
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
