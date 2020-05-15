beb1e368c92434e656009f590bcfa8dd82ab4af8
Edward Li <radioactivehydra@gmail.com>
2020-05-15 19:08:07 -0400

    Added Main Class

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+"""
+Copyright (C) TODO
+"""
+
 import sys
 import argparse
+
+import obsidian
+from obsidian import *
+#from obsidian import server
 
 def main():
     #Initiate Argument Parser
     parser = argparse.ArgumentParser(description="TODO")
-
+    #parser.add_argument('--TEMPLATE', type=int, nargs='?', help='TEMPLATE', default=TEMPLATE)
+    parser.add_argument('-a', '--address', type=str, nargs='?', help='The address the Minecraft server would bind to.', default='localhost')
+    parser.add_argument('-p', '--port', type=int, nargs='?', help='The port the Minecraft server would bind to.', default=25565)
     args = parser.parse_args()
 
-    #Create Server Instance
-    #server = MinecraftServer()
+    #Init Server
+    server = MinecraftServer(args.address, args.port)
+    server.start()
 
 if __name__ == '__main__':
     main()
---
+++ obsidian/__init__.py
@@ -0,0 +1,10 @@
+#Server Version
+__version__ = 'DEV-X.X.X'
+
+class MinecraftServer(object):
+    def __init__(self, address, port):
+        self._address = address
+        self._port = port
+
+    def start(self):
+        print(self._address, self._port)
