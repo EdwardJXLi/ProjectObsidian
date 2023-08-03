a2a2cc78567291cae057a32d4ee29b492d020e94
Edward Li <radioactivehydra@gmail.com>
2023-08-03 13:31:44 -0700

    Improve accuracy of netcode reporting

--- obsidian/modules/netinfo.py
+++ obsidian/modules/netinfo.py
@@ -8,6 +8,7 @@ from obsidian.log import Logger
 from threading import Thread
 import asyncio
 import time
+import math
 
 
 @Module(
@@ -55,25 +56,46 @@ class NetInfoModule(AbstractModule):
         # Create an event loop to run async tasks in
         eventLoop = asyncio.new_event_loop()
 
+        # Last Status
+        lastStatus = time.time()
+
         # Start hot-loop for heartbeat
         while True:
             try:
+                time.sleep(1)
+
                 async def sendNetInfo():
+                    nonlocal lastStatus
+
+                    txPackets = module.txPackets
+                    txBytes = module.txBytes
+                    rxPackets = module.rxPackets
+                    rxBytes = module.rxBytes
+
+                    timeDelta = (time.time() - lastStatus)
+
+                    lastStatus = time.time()
+
+                    txPacketsRate = math.ceil(txPackets / timeDelta)
+                    txBytesRate = math.ceil(txBytes / timeDelta)
+                    rxPacketsRate = math.ceil(rxPackets / timeDelta)
+                    rxBytesRate = math.ceil(rxBytes / timeDelta)
+
                     await MessageTypesModule.sendGlobalMessage(
                         server.playerManager,
-                        f"&cTx: &f{module.txPackets} packets/s &4[{module.txBytes} bytes/s]&f  | ",
+                        f"&cTx: &f{txPacketsRate} packets/s &4[{txBytesRate} bytes/s]&f  | ",
                         messageType=MessageType.BOTTOM_RIGHT_1
                     )
 
                     await MessageTypesModule.sendGlobalMessage(
                         server.playerManager,
-                        f"&aRx: &f{module.rxPackets} packets/s &2[{module.rxBytes} bytes/s]&f  | ",
+                        f"&aRx: &f{rxPacketsRate} packets/s &2[{rxBytesRate} bytes/s]&f  | ",
                         messageType=MessageType.BOTTOM_RIGHT_2
                     )
 
                     await MessageTypesModule.sendGlobalMessage(
                         server.playerManager,
-                        "&dProject&5Obsidian &fNetcode Status",
+                        "&dProject&5Obsidian&f  | ",
                         messageType=MessageType.BOTTOM_RIGHT_3
                     )
 
@@ -83,8 +105,6 @@ class NetInfoModule(AbstractModule):
                     module.txBytes = 0
 
                 eventLoop.run_until_complete(sendNetInfo())
-
-                time.sleep(1)
             except Exception as e:
                 Logger.error(f"Unhandled exception in net info thread - {type(e).__name__}: {e}", module="netinfo", printTb=True)
                 time.sleep(60)
